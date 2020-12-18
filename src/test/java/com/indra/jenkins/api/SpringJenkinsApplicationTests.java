baf58d61161c0f38dcccdcdf7d29bbb354190c35
Indraneel Choudhury <indraneelrkl@gmail.com>
2020-12-18 12:55:24 +0530

    modified test 2

--- src/test/java/com/indra/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/indra/jenkins/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringJenkinsApplicationTests {
+public class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
